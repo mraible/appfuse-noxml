301d3db439971cbd928e0b9ec68077fa41b496d1
Matt Raible <matt@raibledesigns.com>
2015-09-15 02:19:15 -0600

    Removed web.xml.

--- src/main/java/com/raibledesigns/config/SecurityConfig.java
+++ src/main/java/com/raibledesigns/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.raibledesigns.dao.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,7 +12,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-@EnableWebSecurity(prePostEnabled = true, securedEnabled = true)
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserDao userDao;
---
+++ src/main/java/com/raibledesigns/webapp/WebAppInitializer.java
@@ -0,0 +1,80 @@
+package com.raibledesigns.webapp;
+
+import com.opensymphony.sitemesh.webapp.SiteMeshFilter;
+import com.raibledesigns.webapp.filter.LocaleFilter;
+import com.raibledesigns.webapp.jsp.EscapeXmlELResolverListener;
+import com.raibledesigns.webapp.listener.StartupListener;
+import com.raibledesigns.webapp.listener.UserCounterListener;
+import net.sf.navigator.menu.MenuContextListener;
+import org.apache.cxf.transport.servlet.CXFServlet;
+import org.displaytag.filter.ResponseOverrideFilter;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.XmlWebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.filter.DelegatingFilterProxy;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.tuckey.web.filters.urlrewrite.UrlRewriteFilter;
+import ro.isdc.wro.http.WroFilter;
+
+import javax.servlet.*;
+import java.util.EnumSet;
+
+public class WebAppInitializer implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext container) throws ServletException {
+        container.setInitParameter("javax.servlet.jsp.jstl.fmt.localizationContext", "ApplicationResources");
+        container.setInitParameter("javax.servlet.jsp.jstl.fmt.fallbackLocale", "en");
+        container.setInitParameter("contextClass", "org.springframework.web.context.support.AnnotationConfigWebApplicationContext");
+        container.setInitParameter("contextConfigLocation",
+                "com.raibledesigns.config.ResourcesConfig,com.raibledesigns.config.JpaConfig," +
+                "com.raibledesigns.config.ServiceConfig,com.raibledesigns.config.SecurityConfig");
+
+        FilterRegistration.Dynamic encodingFilter = container.addFilter("encodingFilter", CharacterEncodingFilter.class);
+        encodingFilter.setInitParameter("encoding", "UTF-8");
+        encodingFilter.setInitParameter("forceEncoding", "true");
+        encodingFilter.addMappingForUrlPatterns(null, false, "/*");
+
+        FilterRegistration.Dynamic exportFilter = container.addFilter("exportFilter", ResponseOverrideFilter.class);
+        exportFilter.addMappingForUrlPatterns(null, false, "/app/*");
+
+        FilterRegistration.Dynamic localeFilter = container.addFilter("localeFilter", LocaleFilter.class);
+        localeFilter.addMappingForUrlPatterns(null, false, "/*");
+
+        FilterRegistration.Dynamic rewriteFilter = container.addFilter("rewriteFilter", UrlRewriteFilter.class);
+        rewriteFilter.setInitParameter("logLevel", "commons");
+        rewriteFilter.setInitParameter("confReloadCheckInterval", "-1");
+        rewriteFilter.addMappingForUrlPatterns(null, false, "/*");
+
+        FilterRegistration.Dynamic securityFilter = container.addFilter("securityFilter", DelegatingFilterProxy.class);
+        securityFilter.setInitParameter("targetBeanName", "springSecurityFilterChain");
+        securityFilter.addMappingForUrlPatterns(EnumSet.of(
+                DispatcherType.REQUEST, DispatcherType.FORWARD, DispatcherType.INCLUDE), false, "/*");
+
+        FilterRegistration.Dynamic sitemeshFilter = container.addFilter("sitemeshFilter", SiteMeshFilter.class);
+        sitemeshFilter.addMappingForUrlPatterns(EnumSet.of(
+                DispatcherType.REQUEST, DispatcherType.FORWARD), false, "/*");
+
+        FilterRegistration.Dynamic wroFilter = container.addFilter("wroFilter", WroFilter.class);
+        wroFilter.addMappingForUrlPatterns(EnumSet.of(
+                DispatcherType.REQUEST, DispatcherType.FORWARD), false, "/assets/*");
+
+        ServletRegistration.Dynamic cxfServlet = container.addServlet("CXFServlet", CXFServlet.class);
+        cxfServlet.addMapping("/services/*");
+
+        container.addListener(ContextLoaderListener.class);
+        container.addListener(StartupListener.class);
+        container.addListener(UserCounterListener.class);
+        container.addListener(EscapeXmlELResolverListener.class);
+        container.addListener(MenuContextListener.class);
+
+        XmlWebApplicationContext appContext = new XmlWebApplicationContext();
+        appContext.setConfigLocation("/WEB-INF/dispatcher-servlet.xml");
+
+        ServletRegistration.Dynamic dispatcher =
+                container.addServlet("dispatcher", new DispatcherServlet(appContext));
+        dispatcher.setLoadOnStartup(1);
+        dispatcher.addMapping("/app/*");
+    }
+}
